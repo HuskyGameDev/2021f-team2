2193d356db23851507803d688986548247b3a9fc
Maritza Gonzalez <59942843+mcgonzale@users.noreply.github.com>
2021-09-30 10:26:59 -0400

    Health.c, File reorganization
    Created script to start the preliminary health management for the players.
    
    created a scripts folder for the scripts instead of having them floating in the Assets folder

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int maxHealth = 10;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth; //at the beginning of the game, set currentHealth equal to maxHealth
+    }
+
+    public int getHealth()
+    {
+        return currentHealth;
+    }
+
+    public void setHealth() //Passed no arguments, restores health to normal
+    {
+        currentHealth = maxHealth;
+    }
+    public void setHealth(int i)
+    {
+        currentHealth = i;
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth = currentHealth - damage;
+        CheckDeath();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckDeath()   //Called whenever a player takes damage.
+    {
+
+        Debug.Log("Player took damage! Current health is " + currentHealth + "!");//debug code obviously
+
+        if(currentHealth <= 0)
+        {
+            //die die you zombie bastards
+        }
+    }
+}
