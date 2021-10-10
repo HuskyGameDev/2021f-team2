22b60b60c6440455a657542268b7608b11a3cfbf
Jacob Yankee <59894968+VanillaDeluxe@users.noreply.github.com>
2021-10-10 15:21:05 -0400

    tried my best. Added Neco-Arc

--- Assets/moement.cs
+++ Assets/moement.cs
@@ -6,6 +6,7 @@ public class moement : MonoBehaviour
 {
     public float MovementSpeed = 1;
     public float JumpForce = 1;
+    public Animator animator;
 
     private Rigidbody2D _rigidbody;
     // Start is called before the first frame update
@@ -19,10 +20,14 @@ public class moement : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        animator.SetFloat("speed", Mathf.Abs(movement));
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            animator.SetBool("isJump", true);
         }
+
     }
+
 }
